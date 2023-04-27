a35c00e6faa3fa76276c17932fcc2eca5b59463b
Dmitry Demidov <demid088@yandex.ru>
2023-04-28 01:45:37 +0700

    [add] - ImageLoader

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,10 +1,24 @@
+import { createFactory, Fragment } from 'react'
+import Preloader from '../Preloader/Preloader'
+import ImageLoader from 'react-imageloader'
 // styles
 import css from './Collection.module.scss'
 
 function Collection({ name, images }) {
   return (
     <div className={css.collection}>
-      <img className={css.big} src={images[0]} alt='item' />
+      <ImageLoader
+        wrapper={createFactory(Fragment)}
+        preloader={Preloader}
+        src={images[0]}
+        imgProps=
+          {{
+            class: css.big,
+            alt: 'item',
+          }}
+        >
+        Error
+      </ImageLoader>
       <div className={css.bottom}>
         <img className={css.mini} src={images[1]} alt='item' />
         <img className={css.mini} src={images[2]} alt='item' />
---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+// styles
+import css from './Preloader.module.scss'
+
+function Preloader() {
+  return <span className='material-icons-outlined'>pending</span>
+}
+
+export default Preloader
